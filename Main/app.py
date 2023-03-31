a2109e2399bccf0b1a8ba69a1ee4a4b096e8ebc7
Gabriel Quaglio <gabriel.quaglio1304@gmail.com>
2023-03-31 18:28:07 -0300

    comit inicial com códigos na main

--- Main/app.py
+++ Main/app.py
@@ -5,9 +5,11 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+import Funcoes
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 
-
-app = Dash(__name__)
+app = Dash(__name__,assets_folder="Aplicacao/Main/assets/")
 
 # assume you have a "long-form" data frameMy Project 83723
 # see https://plotly.com/python/px-arguments/ for more options
@@ -20,13 +22,52 @@ df_info = pd.read_csv('./Dados_dash/Informações_reabnhos_municipios.csv')
 
 
 app.layout = html.Div(children=[
-    html.H1('Dashboard analitico dos rebanhos brasileiros'),
-    dcc.Dropdown(id='menu-suspenso-UF',
+
+
+    html.H1('Dashboard analitico dos rebanhos brasileiros', className="app-header"),
+
+    html.Div(children = [
+
+        
+        dbc.Row(children = [
+
+                dcc.Dropdown(id='menu-suspenso-UF',
         options=[{'label': valor, 'value': valor} for valor in df_info['SIGLA'].unique()],
-        value=df_info['SIGLA'][0])
+        value=df_info['SIGLA'][0],
+        className='my-dropdown'),
+        
+
+         dcc.Dropdown(
+        id='menu-suspenso-mun',
+        
+        value=''
+    )
 
 
+        
+
+        ])
+
+        
+    ])
 ])
+
+@app.callback(
+    Output('menu-suspenso-mun', 'options'),
+    Input('menu-suspenso-UF', 'value')
+)
+def atualizar_opcoes_cidade(uf_selecionada):
+    opcoes_cidade = [{'label': cidade, 'value': cidade} for cidade in df_info[df_info['SIGLA'] == uf_selecionada]['NM_MUN'].unique()]
+    return opcoes_cidade
+
+
+
+
+
+
+    
+
+
 
 import webbrowser
 webbrowser.open_new('http://localhost:8050/')
